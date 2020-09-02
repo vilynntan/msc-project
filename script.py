aae290e2aa84b12c58045b3ea74c637dd2a02660
vilynntan <46435603+vilynntan@users.noreply.github.com>
2020-09-02 15:11:11 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,29 +1,85 @@
-import os, mmap, subprocess, re
+#Requires python 3 and above
 
-#Compile into class file
-#os.system('javac IntegerBug.java')
+import subprocess, sys
+import networkx as nx
 
-#Find line number where Verifier is
-lineNum = 0
-with open('IntegerBug.java') as f:
-    for num, line in enumerate(f, 1):
-        if 'Verifier.nondet' in line:
-            lineNum = num
-
-print lineNum
+#Extract class name
+classnameArray = sys.argv[1].split('.')
+classname = classnameArray[0]
+if len(classnameArray) == 2 and classnameArray[1] == 'java':
+          subprocess.Popen(['javac', sys.argv[1]]).wait() #Compile Java program
 
 #Run JBMC
-#Require python 2.7 and above
-#os.system('./jbmc IntegerBug --stop-on-fail');
-cmd = './jbmc IntegerBug --stop-on-fail'
+cmd = 'jbmc ' + classname + ' --stop-on-fail --graphml-witness witness'
 try:
      result = subprocess.check_output(cmd, shell=True)
 except subprocess.CalledProcessError as e:
      result = e.output
 
-for line in result.splitlines():
-     if line.startswith('  anonlocal'):
-	print line
-	temp = re.split('=| ', line)
-	print temp[3] #counterexample
-	
+#Check for violation
+witnessFile = nx.read_graphml("witness")
+violation = False
+for violationKey in witnessFile.nodes(data=True):
+     if 'isViolationNode' in violationKey[1]:
+          violation = True
+
+#Violation == True / Violation occurs
+if (violation):
+     #Determine verifier variable type - only possible with .java/user supplied
+     if (len(sys.argv) == 3):
+         type = sys.argv[2].lower()
+     else:
+         with open(sys.argv[1], "rt") as fin:
+             for line in fin:
+                 index = line.find('verifier.') 
+                 if(index != -1):
+                     type = line[index + 15 : -4].lower().replace(')','').replace('(','')
+     
+     #Extract counterexample
+     for data in witnessFile.edges(data=True):
+          if 'assumption' in data[2]:
+               str = data[2]['assumption']
+               #Get counterexample between ' = ' and ';' 
+               #E.g. "anonlocal::1i = 1000;"
+               if (str.startswith('anonlocal')):
+                    counterexample = str.split(' = ')[1][:-1]
+
+     #Create validation harness from template
+     with open("ValidationHarnessTemplate.txt", "rt") as fin:
+         with open("ValidationHarness.java", "wt") as fout:
+             for line in fin:
+                 line = line.replace('ClassName', classname)
+                 if(type == 'int'):
+                     line = line.replace('Type', 'nondetInt').replace('Counterexample', counterexample)
+                 if(type == 'short'):
+                     line = line.replace('Type', 'nondetShort').replace('Counterexample', counterexample)
+                 if(type == 'long'):
+                     line = line.replace('Type', 'nondetLong').replace('Counterexample', counterexample)
+                 if(type == 'float'):
+                     line = line.replace('Type', 'nondetFloat').replace('Counterexample', counterexample)
+                 if(type == 'double'):
+                     line = line.replace('Type', 'nondetDouble').replace('Counterexample', counterexample)
+                 if(type == 'string'):
+                     try:
+                         counterexample = int(counterexample)
+                         line = line.replace('Type', 'nondetString').replace('Counterexample', 'null')
+                     except ValueError:
+                         line = line.replace('Type', 'nondetString').replace('Counterexample', '"' + counterexample + '"')
+                 if(type == 'char'):
+                     line = line.replace('Type', 'nondetChar').replace('Counterexample', '\'' + chr(int(counterexample)) + '\'')
+                 if(type == 'boolean'):
+                     if(counterexample == '1'):
+                         line = line.replace('Type', 'nondetBoolean').replace('Counterexample', 'true')
+                     if(counterexample == '0'):
+                         line = line.replace('Type', 'nondetBoolean').replace('Counterexample', 'false')
+                 fout.write(line)
+
+     #Compile validation harness
+     subprocess.Popen(['javac', 'ValidationHarness.java']).wait()
+
+     #Execute validation harness
+     subprocess.Popen(['java', '-ea', 'ValidationHarness']).wait()
+else:
+     #Exit if no violation found by JBMC
+     print ('No violation found')
+     exit(1)
